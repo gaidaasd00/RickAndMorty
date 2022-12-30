e0a57efc4603477ef31c0b070bf5dc906f55a0ca
Alexey Gaidykov <swiftgaida@list.ru>
2022-12-30 08:45:23 +0300

    Settings cells

---
+++ RickAndMorty/Resourses/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  RickAndMorty
+//
+//  Created by Alexey Gaidykov on 29.12.2022.
+//
+
+import UIKit
+
+extension UIView {
+    func addSubview(_ views: UIView...) {
+        views.forEach({
+            addSubview($0)
+        })
+    }
+}
